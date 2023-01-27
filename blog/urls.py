0b0b6ae869ba05cf79cac5c6c1e5120d1b35a3e4
Yeison <yebq.adm@gmail.com>
2023-01-26 22:36:32 -0500

    Creando get y post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BlogLisView
+from .views import BlogLisView, BlogCreateView
 
 app_name="blog"
 
 urlpatterns = [
-    path('', BlogLisView.as_view(), name="home")    
+    path('', BlogLisView.as_view(), name="home"),
+    path('create/', BlogCreateView.as_view(), name="create")    
 ]
+
